40b85f7630cb611562ad13691326efc12725730c
Константин <koxa48@gmail.com>
2022-04-25 17:40:51 +0600

    Добавил возможность указания нескольких директорий

--- backup/backup_ver1.py
+++ backup/backup_ver1.py
@@ -1,24 +1,29 @@
+from ast import walk
 from zipfile import ZipFile
 import os
 import time
 
-catalog = 'F:\Загрузки\ЗагрузкиКости\wallhaven'
+#TODO: add progress bar
+
+catalogs = ['F:\Загрузки\ЗагрузкиКости\wallhaven', 'F:\Kostya\desktop']
 
 target_dir = 'F:\Kostya\Backup'
 
-path_zip = target_dir + os.sep + time.strftime('%Y%m%d') + '.zip'
+path_zip = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 
-#zip_command = 'zip -qr {0} {1}'.format(target_dir, ' '.join(catalogs))
+def get_all_file_path(dirs):
+    file_path = []
+    for dir in dirs:
+        for root, directorias, files in os.walk(dir):
+            for file_name in files:
+                file_path.append(os.path.join(root, file_name))
+    return file_path
 
-# print(zip_command)
-# if os.system(zip_command) == 0:
-#     print('Резервная копия успешно создана в', path_zip)
-# else:
-#     print('Создание резервной копии не удалось')
-file_dir = os.listdir(catalog)
-with ZipFile(path_zip, 'a') as zip:
-    for file in file_dir:
-        zip.write(os.path.join(catalog, file))
+    
+file_names = get_all_file_path(catalogs)
 
+with ZipFile(path_zip, 'w') as zip:
+    for file in file_names:
+        zip.write(file)
 
 print('Резервная копия успешно создана в', path_zip)

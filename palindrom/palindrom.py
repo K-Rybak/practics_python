59a334d98dc81b02eb41db4522b7f4413aba959f
Константин <koxa48@gmail.com>
2022-04-26 14:54:24 +0600

    Добавил удаление не нужных символов из стороки и гнорирование регистра

--- palindrom/palindrom.py
+++ palindrom/palindrom.py
@@ -4,9 +4,16 @@ def reverse(text):
 def isPalindrom(text):
     return reverse(text) == text
 
+def del_forbidden_signs(text):
+    forbidden_signs = ' ', '!', '?', ',', '.'
+
+    for sign in forbidden_signs:
+        text = text.replace(sign, '')
+    return text
+
 string = input('Ввелите текст: ')
 
-if isPalindrom(string):
+if isPalindrom(del_forbidden_signs(string.lower())):
     print('Да это палидром')
 else:
     print('Нет, это не палидром')
